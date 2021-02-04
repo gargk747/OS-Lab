b140f5a12199bc0c24540e0447ca8576592d520c
gargk747 <gargk747@gmail.com>
2021-02-04 17:09:28 +0530

    Assignment 3 done

---
+++ Assignment 3/python scheduler file/process.py
@@ -0,0 +1,33 @@
+class Process:
+	# name, priority, arrival time, list of burst time & I/O time
+	def __init__(self, data):
+		# pre-processing
+		data=data.split(" ")
+		while '' in data:
+			data.remove('')
+		# pid,AT,priority,share,instructions
+		self.pid = int(data[0])
+		self.arrivalTime = int(data[1])
+		self.priority = int(data[2])
+		self.share=int(data[3])
+		self.state = "start"
+		self.xtimer = -1
+		self.waitingTime = 0
+		self.turnAroundTime = 0
+		self.responseTime = -1
+		self.endTime = 0
+		self.burstTime=0
+		self.inProcess = False
+		self.instructions = list()
+		i=4
+		while data[i]!='-1':
+			self.instructions.append([data[i],int(data[i+1])])
+			self.burstTime+=int(data[i+1])
+			i+=2
+		self.remainingTime = self.burstTime
+	def invertPriority(self, other):
+		self.priority, other.priority = other.priority, self.priority
+	def increasePriority(self):
+		self.priority -= 1
+	def decreasePriority(self):
+		self.priority += 1
